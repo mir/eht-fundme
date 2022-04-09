c649f383a0f08a4bbcbb545c257faec98b1c34e7
Marat Yuldashev <marat.spb@protonmail.com>
2022-04-09 23:48:30 +0300

    Pushed to mainnet fork

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -3,10 +3,12 @@ from web3 import Web3
 
 DECIMALS = 8
 STARTING_PRICE = 2000e8
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-forked", "mainnet-fork-dev"]
 LOCAL_BLOCKHAIN_ENVIRONMENTS = ["development","ganache-local"]
 
 def get_account():
-    if (network.show_active() in LOCAL_BLOCKHAIN_ENVIRONMENTS):
+    if (network.show_active() in LOCAL_BLOCKHAIN_ENVIRONMENTS 
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,6 +1,9 @@
+import pytest
+from scripts.fund_and_withdraw import fund_me
 from scripts.helpful_scripts import get_account
-from scripts.deploy import deploy_fund_me
-from brownie import accounts, FundMe
+from scripts.deploy import deploy_fund_me, LOCAL_BLOCKHAIN_ENVIRONMENTS
+from brownie import accounts, network, exceptions
+
 
 def test_fund_and_withdraw():
     account = get_account()
@@ -11,3 +14,12 @@ def test_fund_and_withdraw():
     assert fund_me.addressToAmountFunded(account.address) == entrance_fee
     tx2 = fund_me.withdraw({"from": account})
     assert fund_me.addressToAmountFunded(account.address) == 0
+
+def test_only_withdraw():
+    if network.show_active() not in LOCAL_BLOCKHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing")    
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor })
+        
