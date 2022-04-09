08ec988083512ca767bffc5864b60d76bd4c1e5e
Marat Yuldashev <marat.spb@protonmail.com>
2022-04-09 09:39:01 +0300

    Ganache-local

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,27 +1,24 @@
 from brownie import FundMe, MockV3Aggregator, config, accounts, network
-from scripts.helpful_scripts import get_account
-
-
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKHAIN_ENVIRONMENTS)
 
 def main():
     deploy_fund_me()
 
-
 def deploy_fund_me():
     account = get_account()
     print(f"Active network is {network.show_active()}")
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
     else:
-        print(f"Active network is {network.show_active}")
-        print("Deploying mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(18,2000e18,{"from": account})
-        print("Mocks deployed")
-        price_feed_address = mock_aggregator.address
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1]  
 
     fund_me = FundMe.deploy(
         price_feed_address, # price feed address
         {"from": account},
-         publish_source=config["networks"][network.show_active()]["verify"]
+         publish_source=config["networks"][network.show_active()].get("verify")
         )
     print(f"Contract is deployed to {fund_me.address}")
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,8 +1,22 @@
-from brownie import accounts, config, network
+from brownie import accounts, config, network, MockV3Aggregator
+from web3 import Web3
 
+DECIMALS = 18
+STARTING_PRICE = 2000
+LOCAL_BLOCKHAIN_ENVIRONMENTS = ["development","ganache-local"]
 
 def get_account():
-    if (network.show_active() == 'development'):
+    if (network.show_active() in LOCAL_BLOCKHAIN_ENVIRONMENTS):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+def deploy_mocks():
+    print(f"Active network is {network.show_active}")
+    print("Deploying mocks...")
+    if (len(MockV3Aggregator) <= 0):
+        mock_aggregator = MockV3Aggregator.deploy(
+            DECIMALS, # Decimals
+            Web3.toWei(STARTING_PRICE, "ether"), # Cost (wei)
+            {"from": get_account()})
+        print("MockV3Aggregator is deployed")
